5b9b877400736a1d745f13e4961f020ffc098079
FinnWeng <finn.weng@gmail.com>
2019-09-09 16:49:22 +0800

    first commit

---
+++ A2C/RL_model.py
@@ -0,0 +1,204 @@
+import gym
+import tensorflow as tf
+import numpy as np
+
+
+class RL_model:
+    def __init__(self, input_shape, seq_size=4, baseline=0.0, batch_size=32, TD_traj_leng=5, discount_rate=0.99,
+                 training_LR=1e-6, ent_coef=0.0005, vf_coef=2):
+        self.seq_size = seq_size  # MUST greater than 2. How many past states should we look for decide an action
+
+        self.baseline = baseline
+
+        self.batch_size = batch_size
+
+        self.TD_traj_leng = TD_traj_leng  # actually past design is TD 1, now I assign it to estimate more step
+
+        self.discount_rate = discount_rate
+
+        self.training_LR = training_LR
+
+        self.ent_coef = ent_coef
+
+        self.vf_coef = vf_coef
+
+        self.input_shape = input_shape
+
+        self.build_net()
+
+    def build_net(self):
+
+        with tf.name_scope("inputs"):
+            input_state = tf.placeholder(shape=[210, 160, 3], dtype=tf.uint8)
+
+            x_holder = tf.placeholder(tf.float32, self.input_shape,
+                                      name="states")  # reduce batch and timestep when input
+            # value_y_holder = tf.placeholder(tf.float32, [None, 1], name="value_y")  # reward, None => how long it play
+
+            actions_y_holder = tf.placeholder(tf.float32, [None, 4],
+                                              name="action_y")  # action it have taken,[batch_size, 4]
+            R_plus_plus1_v_holder = tf.placeholder(tf.float32, [None, 1], name="R_plus_plus1_v")  # [batch_size,1]
+            episode_reward_holder = tf.placeholder(tf.float32, [], name="episode_reward")
+
+        # define action op before loop
+        # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+        encoder_output = self.encoder_net(x_holder)  # (-1,210,160,3) feed an array of images, -1 = batch_size
+        logits = self.actor_net(encoder_output)  # (batch_size,4 actions)
+        prediction_prob = tf.add(tf.nn.softmax(logits), 1e-8)  # (batch_size,4 prob)
+
+        print("prediction_prob:", prediction_prob.shape)
+
+        # define critic op before loop
+        # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+        V_value = self.critic_net(encoder_output)  # batch_size, 1 value
+
+        # RGB to Gray
+        gray_img_output = tf.image.rgb_to_grayscale(input_state)
+
+        # define actor training op before loop
+        ########################################################
+
+        # st
+
+        entropy = -tf.reduce_sum(prediction_prob * tf.log(prediction_prob),
+                                 name="entropy")  # batchsize, 1
+
+        neglogpac = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
+                                                                   labels=tf.argmax(actions_y_holder, axis=1))
+
+        advantage = R_plus_plus1_v_holder - V_value
+
+        # policy_gradient = tf.multiply(advantage,
+        #                               log_prob) + 0.001 * entropy  # (batch_size, 1 Q value))*(batch_size, 1 max prob) + (batch_size, 1 entropy)
+
+        actor_loss = tf.reduce_mean(advantage * neglogpac)  # policy_gradient_loss
+
+        # noisy_net
+
+        noisy_net_ops = []
+
+        target_net_only_variables = [v for v in tf.global_variables() if "actor" in v.name]
+
+        for pre_noise_variable in target_net_only_variables:
+            noisy_net_noise = tf.random.normal(shape=pre_noise_variable.shape)
+            noisy_net_ops.append(tf.add(pre_noise_variable, noisy_net_noise))
+
+        # define critic training op before loop
+        ###############################################
+
+        critic_loss = tf.losses.mean_squared_error(R_plus_plus1_v_holder,
+                                                   V_value)
+
+        total_loss = actor_loss - entropy * self.ent_coef + critic_loss * self.vf_coef
+        # cross_entropy is minimize,  - entropy is minimize, critic loss is minimize
+
+        optimizer = tf.train.AdamOptimizer(self.training_LR, name="adam")
+        #
+        # variables = [v for v in tf.global_variables()]
+        #
+        # gradients, variables_beside_gd = zip(
+        #     *optimizer.compute_gradients(actor_loss, var_list=variables))
+        #
+        # clipped_gradients = [tf.clip_by_value(gradient, -1., 1.) for gradient in gradients]
+        #
+        # total_training_op = optimizer.apply_gradients(zip(clipped_gradients, variables))
+
+        total_training_op = optimizer.minimize(total_loss)
+
+        max_advantage = tf.reduce_max(advantage)
+        min_advantage = tf.reduce_min(advantage)
+        avg_advantage = tf.reduce_mean(advantage)
+        max_V_value = tf.reduce_max(V_value)
+        min_V_value = tf.reduce_min(V_value)
+        avg_V_value = tf.reduce_mean(V_value)
+
+        summary_figure = [tf.summary.scalar("actor_loss", actor_loss),
+                          tf.summary.scalar("critic_loss", critic_loss),
+                          tf.summary.scalar("entropy", entropy),
+                          tf.summary.histogram("actor_loss_hist", actor_loss),
+                          tf.summary.histogram("critic_loss_hist", critic_loss),
+                          tf.summary.scalar("total_loss", total_loss),
+                          tf.summary.scalar("max_advantage", max_advantage),
+                          tf.summary.scalar("min_advantage", min_advantage),
+                          tf.summary.scalar("avg_advantage", avg_advantage),
+                          tf.summary.scalar("max_V_value", max_V_value),
+                          tf.summary.scalar("min_V_value", min_V_value),
+                          tf.summary.scalar("avg_V_value", avg_V_value)
+                          ]
+
+        # outputs
+        self.input_state = input_state
+        self.x_holder = x_holder
+        self.actions_y_holder = actions_y_holder
+        self.R_plus_plus1_v_holder = R_plus_plus1_v_holder
+        self.episode_reward_holder = episode_reward_holder
+        self.summary_figure = summary_figure
+        self.total_training_op = total_training_op
+        self.gray_img_output = gray_img_output
+        self.V_value = V_value
+        self.prediction_prob = prediction_prob
+        self.advantage = advantage
+        self.actor_loss = actor_loss
+        self.critic_loss = critic_loss
+        self.total_loss =total_loss
+
+    def encoder_net(self, enc_x_holder):
+        layer_count = 0
+        gain = np.sqrt(2)
+        x_holder_straight = tf.reshape(enc_x_holder, [-1, 210, 160, 1])
+
+        normalized_x = (x_holder_straight) / 255
+
+        def conv(inputs, nf, ks, strides, gain=1.0):
+            return tf.layers.conv2d(inputs=inputs, filters=nf, kernel_size=ks,
+                                    strides=(strides, strides), activation=tf.nn.relu,
+                                    kernel_initializer=tf.orthogonal_initializer(gain=gain),
+                                    name="enc_net_layer%s" % (layer_count),
+                                    reuse=tf.AUTO_REUSE)
+
+        h1 = conv(normalized_x, 32, 8, 4, gain)
+        layer_count += 1
+        h2 = conv(h1, 64, 4, 2, gain)
+        layer_count += 1
+        h3 = conv(h2, 64, 3, 1, gain)
+        layer_count += 1
+        encoder_output_flat = tf.layers.flatten(h3)
+
+        return encoder_output_flat
+
+    def actor_net(self, encoder_output_flat):  # num_step = seq_size
+
+        gain = 1.0
+
+        h_nn3 = tf.layers.dense(inputs=encoder_output_flat, units=512, activation=tf.nn.relu,
+                                kernel_initializer=tf.orthogonal_initializer(gain), name="actor_net_dense3",
+                                reuse=tf.AUTO_REUSE)
+
+        # nn layer 4
+        logits = tf.layers.dense(h_nn3, 4, use_bias=True,
+                                 name="actor_net_dense4",
+                                 reuse=tf.AUTO_REUSE)
+
+        print("logits:", logits)
+
+        return logits
+
+    def critic_net(self, encoder_output_flat):  # num_step = seq_size
+
+        gain = 1.0
+
+        h_nn3 = tf.layers.dense(inputs=encoder_output_flat, units=512, activation=tf.nn.relu,
+                                kernel_initializer=tf.orthogonal_initializer(gain), name="critic_net_dense3",
+                                reuse=tf.AUTO_REUSE)
+
+        # nn layer 4
+        V_value = tf.layers.dense(h_nn3, 1,
+                                  kernel_initializer=tf.orthogonal_initializer(gain),
+                                  name="critic_net_dense4",
+                                  reuse=tf.AUTO_REUSE)
+
+        print("V_value:", V_value)
+
+        return V_value
---
+++ A2C/breakout_TD_A2C.py
@@ -0,0 +1,241 @@
+import gym
+import tensorflow as tf
+import numpy as np
+import gc
+# from os import getpid
+import os
+import time
+import random
+import collections
+import pprint
+
+import RL_model
+
+
+
+if __name__ == "__main__":
+
+    logs_path = "./A2C/tf_log/"
+
+    critic_loss_history = []
+    actor_loss_history = []
+    neg_entropy_history = []
+
+    replay_buffer = []
+
+    # use which GPU device
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+    env = gym.make('Breakout-v0')
+
+    seq_size = 4  # MUST greater than 2. How many past states should we look for decide an action
+
+    baseline = 0.0
+
+    batch_size = 32
+
+    TD_traj_leng = 5  # actually past design is TD 1, now I assign it to estimate more step
+
+    discount_rate = 0.99
+
+    update_times = 0
+
+    training_LR = 1e-6
+
+    ent_coef = 0.0005
+
+    vf_coef = 2
+
+    input_shape = [None, 210, 160, 1]
+
+    model = RL_model.RL_model(input_shape,seq_size, baseline, batch_size, TD_traj_leng, discount_rate,
+                 training_LR, ent_coef, vf_coef)
+
+    init = tf.global_variables_initializer()
+
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(list(tf.global_variables()))
+
+    learning_figures = model.summary_figure
+
+    merged_summary_op = tf.summary.merge(learning_figures)
+    episode_reward_summary_op = tf.summary.merge([tf.summary.scalar("episode_reward", model.episode_reward_holder)])
+
+    with tf.Session() as sess:
+
+        # train in firsttime
+        sess.run(init)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=1)
+
+        # # # keep training
+        # saver = tf.train.Saver(max_to_keep=1)
+        # model_file = tf.train.latest_checkpoint('./A2C/model/')
+        # saver.restore(sess, model_file)
+
+        # op to write logs to Tensorboard
+        summary_writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
+
+        noise_target_prob = 50
+
+        for i_episode in range(int(304766),10000000):
+
+            # if i_episode > 0 and i_episode % 20 == 0:
+            #     noise_target_prob = 500.
+            #     print("do all episode random sample!!")
+
+            # train_sample_buffer = []
+
+            episode_reward = 0.
+            inner_train_sample_buffer = []
+            train_buffer = []
+            TD_N_reward = []
+            training_policy_target = []
+            training_action = []
+            training_st = []
+            training_p1_st = []
+
+            # start a round
+            observation = env.reset()
+            observation = sess.run([model.gray_img_output], feed_dict={model.input_state: observation})[0]
+
+            # step_size = 100000
+            step_size = 10000
+
+            # sess.run(noisy_net_ops)
+
+            for step in range(step_size):
+                training_step = step - ((TD_traj_leng - 1 + 1))
+                # seq_size + TD_traj_leng - 1, -1 for TD cummulating, +1 for states+1
+
+                env.render()
+
+                # # temp include st, at, rt, st+1
+                temp = {"st": observation}  # temp append st
+
+                action_prob = sess.run(model.prediction_prob,
+                                       feed_dict={model.x_holder: observation.reshape([1, 210, 160, 1])})  # generate action
+                action_prob += 1e-8
+
+                print("action_prob[0]", action_prob[0])
+
+                # take action
+                action_one_hot = np.zeros([4])
+                # action = np.argmax(action_prob, axis=1)
+                # print("action", action)
+                action = np.random.choice(np.arange(len(action_prob[0])), p=action_prob[0])
+
+                # # # epsilon noise (type2)
+                # if noise_target_prob > 50:
+                #     noise_target_prob -= 0.5
+                #     print("random rate decreasing...")
+                #
+                # noise_threshold = 1000 - noise_target_prob
+                # if np.random.randint(0, 1000, dtype=int) > noise_threshold:
+                #     print("do random sample!!!!")
+                #     action = [env.action_space.sample()]
+
+                print("action", action)
+
+                action_one_hot[action] = 1  # turn predict prob to one hot
+                observation, reward, done, info = env.step(action)  # get result of action
+                observation = sess.run([model.gray_img_output], feed_dict={model.input_state: observation})[0]
+                p1_observation = observation
+
+                episode_reward += reward
+
+                # after action net decide how to take action and get reward, append at, rt
+
+                temp["rt"] = reward
+                temp["at"] = action_one_hot
+                temp["done"] = done
+                temp["p1_st"] = p1_observation
+
+                # append temp to train sample buffer
+
+                inner_train_sample_buffer.append(temp)
+
+                if training_step >= 0:
+
+                    # to compute one TD loss
+
+                    training_reward = 0.0
+                    policy_target = 0.0
+
+                    if not inner_train_sample_buffer[-1]["done"]:
+                        policy_target = sess.run(model.V_value, feed_dict={
+                            model.x_holder: inner_train_sample_buffer[-1]["p1_st"].reshape(
+                                [-1, 210, 160, 1])})[0][0]  # plus1 state value
+
+                    critic_targets = []
+                    actions = []
+
+                    for transition in inner_train_sample_buffer[:-(TD_traj_leng + 1):-1]:
+                        training_reward = transition["rt"] + discount_rate * training_reward
+                        policy_target = transition["rt"] + discount_rate * policy_target
+
+                    TD_N_reward.append(training_reward)
+                    training_policy_target.append(policy_target)
+                    training_action.append(inner_train_sample_buffer[-(TD_traj_leng)]["at"])
+                    training_st.append(inner_train_sample_buffer[-(TD_traj_leng)]["st"])
+                    training_p1_st.append(inner_train_sample_buffer[-1]["p1_st"])
+
+                    if len(training_policy_target) == 2 * batch_size or done == 1:
+                        TD_N_reward_array = np.array(TD_N_reward[-batch_size:]).reshape(-1, 1)
+                        training_policy_target_array = np.array(training_policy_target[-batch_size:]).reshape(-1, 1)
+                        training_action_array = np.array(training_action[-batch_size:])
+                        training_st_array = np.array(training_st[-batch_size:])
+                        training_p1_st_array = np.array(training_p1_st[-batch_size:])
+                        print("TD_N_reward:", TD_N_reward_array.shape)
+                        print("training_policy_target_array :", training_policy_target_array.shape)
+                        print("training_action:", training_action_array.shape)
+                        print("training_st:", training_st_array.shape)
+                        print("training_p1_st:", training_p1_st_array.shape)
+
+                        # sess.run(noisy_net_ops)
+
+                        step_feed_dict = {model.x_holder: training_st_array,
+                                          model.R_plus_plus1_v_holder: training_policy_target_array,
+                                          model.actions_y_holder: training_action_array}
+
+                        # encoder_output_real = sess.run(encoder_output, feed_dict=step_feed_dict)
+                        # print("encoder_output_real:", encoder_output_real.shape)
+
+                        V_value_real = sess.run([model.V_value], feed_dict=step_feed_dict)
+                        print("V_value_real:",V_value_real)
+                        # print("total_training_end!!")
+
+                        _, actor_loss_real, critic_loss_real, total_loss_real, advantage_real, summary = sess.run(
+                            [model.total_training_op, model.actor_loss, model.critic_loss, model.total_loss, model.advantage, merged_summary_op],
+                            feed_dict=step_feed_dict
+                        )
+                        print("advantage_real:", advantage_real)
+
+                        summary_writer.add_summary(summary, global_step=update_times)
+
+                        # when update count reach limit, update the target net variable
+
+                        # sess.run(noisy_net_ops)
+                        update_times += 1
+
+                        del TD_N_reward[:batch_size]
+                        del training_policy_target[:batch_size]
+                        del training_action[:batch_size]
+                        del training_st[:batch_size]
+                        del training_p1_st[:batch_size]
+
+                        # sess.run(noisy_net_ops)
+
+                # gc.collect()
+                print("i_episode:", i_episode)
+                print("step:", step)
+
+                if done:
+                    episode_reward_summary = sess.run([episode_reward_summary_op],
+                                                      feed_dict={model.episode_reward_holder: np.array(episode_reward)})
+                    summary_writer.add_summary(episode_reward_summary[0], global_step=i_episode)
+                    print("Episode finished after {} timesteps".format(step))
+                    break
+
+            # Save the variables to disk.
+            save_path = saver.save(sess, "./A2C/breakout_A2C_model.ckpt", global_step=i_episode)
+            print("Model saved in path: %s" % save_path)
